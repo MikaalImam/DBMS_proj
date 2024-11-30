a25c6a05c33477489aeca8c03ee8e4d84f481c38
Ameer Hamza <ab09162@st.habib.edu.pk>
2024-11-30 20:33:59 +0500

    nigg

--- main(Hamza).py
+++ main(Hamza).py
@@ -69,7 +69,7 @@ class LoginPage(QtWidgets.QMainWindow):
             if cursor.fetchval() == True:
                 print("OPS")
                 #show the ops screen
-                # self.ops_homepage = Ops_Homepage()
+                self.ops_homepage = Ops_Homepage()
                 self.ops_homepage.show()
             else:
                 print("HR")
@@ -97,6 +97,79 @@ class HR_Homepage(QtWidgets.QMainWindow):
         
     def Emp_management(self):
         print("Employees")
+        self.g_table = guard_table()
+        self.g_table.show()
+        
+class guard_table(QtWidgets.QMainWindow):
+    def __init__(self):
+        super(guard_table, self).__init__()
+        uic.loadUi("Gaurd table.ui", self)
+        self.populate_g_table()
+        self.pushButton_3.clicked.connect(self.close)
+        self.pushButton.clicked.connect(self.search_emp)
+        # self.pushButton_2.clicked.connect(self.update_emp)
+        
+    def populate_g_table(self):
+        connection = pyodbc.connect(connection_string)
+        cursor = connection.cursor()
+        select_query = """
+                        select G.Guard_id, A.F_Name + ' ' + A.L_Name as Name, A.Contact_No, A.Height, A.Weight
+                        from Guard G join Application A on G.CNIC = A.CNIC
+                        """
+        cursor.execute(select_query)
+
+        for row_index, row_data in enumerate(cursor.fetchall()):
+            self.tableWidget.insertRow(row_index)
+            for col_index, cell_data in enumerate(row_data):
+                item = QTableWidgetItem(str(cell_data))
+                self.tableWidget.setItem(row_index, col_index, item)
+
+        connection.close()
+        
+        header = self.tableWidget.horizontalHeader()
+        header.setSectionResizeMode(0, QHeaderView.ResizeMode.Stretch)
+        header.setSectionResizeMode(1, QHeaderView.ResizeMode.Stretch)
+        header.setSectionResizeMode(2, QHeaderView.ResizeMode.Stretch)
+        header.setSectionResizeMode(3, QHeaderView.ResizeMode.Stretch)
+        header.setSectionResizeMode(4, QHeaderView.ResizeMode.Stretch)
+        
+    def search_emp(self):
+        guard_id = self.lineEdit.text()
+        name = self.lineEdit_2.text()
+    
+        connection = pyodbc.connect(connection_string)
+        cursor = connection.cursor()
+    
+        select_query = """ 
+                        SELECT G.Guard_id, A.F_Name + ' ' + A.L_Name AS Name, A.Contact_No, A.Height, A.Weight 
+                        FROM Guard G JOIN Application A ON G.CNIC = A.CNIC 
+                        WHERE G.Guard_id = ? OR A.F_Name LIKE ? OR A.L_Name LIKE ? 
+                       """
+    
+        search_params = [guard_id, f'%{name}%', f'%{name}%']
+    
+        cursor.execute(select_query, search_params)
+    
+        
+        self.tableWidget.setRowCount(0)
+    
+        for row_index, row_data in enumerate(cursor.fetchall()):
+            self.tableWidget.insertRow(row_index)
+            for col_index, cell_data in enumerate(row_data):
+                item = QTableWidgetItem(str(cell_data))
+                self.tableWidget.setItem(row_index, col_index, item)
+    
+        connection.close()
+    
+        header = self.tableWidget.horizontalHeader()
+        header.setSectionResizeMode(0, QHeaderView.ResizeMode.Stretch)
+        header.setSectionResizeMode(1, QHeaderView.ResizeMode.Stretch)
+        header.setSectionResizeMode(2, QHeaderView.ResizeMode.Stretch)
+        header.setSectionResizeMode(3, QHeaderView.ResizeMode.Stretch)
+        header.setSectionResizeMode(4, QHeaderView.ResizeMode.Stretch)        
+    
+    # def update_emp(self):
+    #     end
         
 class new_applicant(QtWidgets.QMainWindow):
     def __init__(self):
@@ -105,7 +178,7 @@ class new_applicant(QtWidgets.QMainWindow):
         self.pushButton_3.clicked.connect(self.close)
         self.pushButton_4.clicked.connect(self.insert_applicant)
         
-    def insert_applicant(self):
+    def insert_applicant(self, i_username):
         f_name = self.lineEdit.text()
         l_name = self.lineEdit_2.text()
         cnic = self.lineEdit_3.text()
@@ -117,17 +190,36 @@ class new_applicant(QtWidgets.QMainWindow):
         dob_qdate = self.dateEdit.date()
         dob = dob_qdate.toString("yyyy-MM-dd")
         experience = self.comboBox.currentText()
-        status = "Accepted" if self.radioButton.isChecked() else "Rejected"
-    
+        status = 1 if self.radioButton.isChecked() else 0
+
         connection = pyodbc.connect(connection_string)
         cursor = connection.cursor()
-        insert_query = """
-                    INSERT INTO Application (CNIC, F_Name, L_Name, DOB, Contact_No, Emergency_No, Address, Weight, Height, Experience_in_years, Status) 
-                    VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
-                   """
-    
-        cursor.execute(insert_query, (cnic, f_name, l_name, dob, contact_num, emergency_num, address, weight, height, experience, status))
-        connection.commit()
+
+        # Query to insert into the Application table
+        insert_application_query = """
+                                    INSERT INTO Application (CNIC, F_Name, L_Name, DOB, Contact_No, Emergency_No, Address, Weight, Height, Experience_in_years, Status)
+                                    VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+                                   """
+        cursor.execute(insert_application_query, (cnic, f_name, l_name, dob, contact_num, emergency_num, address, weight, height, experience, status))
+
+        # Query to insert into the Emp_App table
+        insert_emp_app_query = """
+                                INSERT INTO Emp_App (Emp_id, CNIC)
+                                VALUES (?, ?)
+                               """
+        cursor.execute(insert_emp_app_query, (i_username, cnic))
+
+        # Insert into the Guard table if status is 1
+        if status == 1:
+            insert_guard_query = """
+                                 INSERT INTO Guard (CNIC, BankAccount)
+                                 VALUES (?, NULL)
+                                 """
+            cursor.execute(insert_guard_query, (cnic,))
+
+            connection.commit()
+
+
 
 
 def main():
